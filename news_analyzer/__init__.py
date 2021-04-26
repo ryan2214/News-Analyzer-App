39c3f1599496c6e14af6c489f51063959db2c2ce
ryan2214 <ryan2214@users.noreply.github.com>
2021-04-26 12:47:26 +0800

    upload complete

--- news_analyzer/__init__.py
+++ news_analyzer/__init__.py
@@ -1,11 +1,12 @@
 import os
 from flask import Flask
 
-UPLOAD_FOLDER = './upload'
+basepath = os.path.dirname(__file__)
+CURRENT_FOLDER = basepath
 
 app = Flask(__name__)
 app.secret_key = "secret key"
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['CURRENT_FOLDER'] = CURRENT_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 basedir = os.path.abspath(os.path.dirname(__file__))
 
--- news_analyzer/file_uploader.py
+++ news_analyzer/file_uploader.py
@@ -25,22 +25,22 @@ def index():
 
 @bp.route('/', methods=['POST'])
 def upload_file():
-	if request.method == 'POST':
-		if 'file' not in request.files:
-			flash('No file part')
-			return redirect(request.url)
-		file = request.files['file']
-		if file.filename == '':
-			flash('No file selected for uploading')
-			return redirect(request.url)
-		if file and allowed_file(file.filename):
-			filename = secure_filename(file.filename)
-			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-			flash('File successfully uploaded')
-			return redirect('/')
-		else:
-			flash('Allowed file types are txt, pdf')
-			return redirect(request.url)
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']          # file as handle
+        if file.filename == '':
+            flash('No file selected for uploading')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['CURRENT_FOLDER'],'upload', filename))
+            flash('File successfully uploaded')
+            return redirect('/')
+        else:
+            flash('Allowed file types are txt, pdf')
+            return redirect(request.url)
         
 @bp.route('/file/metadata/<file>', methods=['GET'])
 def extract_metadata(file):
